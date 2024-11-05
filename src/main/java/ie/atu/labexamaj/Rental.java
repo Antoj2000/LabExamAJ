f71c42ff6bbf60584e94a2b344674eecb80b02bf
Antoj2000 <g00385306@atu.ie>
2024-11-05 11:41:05 +0000

    Controller class created

--- src/main/java/ie/atu/labexamaj/Rental.java
+++ src/main/java/ie/atu/labexamaj/Rental.java
@@ -4,32 +4,33 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class Rental {
 
     @NotBlank(message = "Rental code must be entered")
-    public String rentalCode;
+    private String rentalCode;
 
     @Max(value = 50, message = "Max 50 characters")
-    public String carModel;
+    private String carModel;
 
     @Max(value = 100, message = "Max 100 characters")
-    public String renterName;
+    private String renterName;
 
     @Email(message = "Must be a valid email format")
-    public String renterEmail;
+    private String renterEmail;
 
 
-    public String rentalStartDate;
+    private String rentalStartDate;
 
-    public String rentalEndDate;
+    private String rentalEndDate;
 
     @Positive(message = "Must be a positive decimal value")
-    public double dailyRate;
+    private double dailyRate;
 
 }
--- src/main/java/ie/atu/labexamaj/rentalController.java
+++ src/main/java/ie/atu/labexamaj/rentalController.java
@@ -1,10 +1,38 @@
 package ie.atu.labexamaj;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("rentals")
 public class rentalController {
+
+    private List<Rental> rentalList = new ArrayList<>();
+
+    public rentalController(){
+
+        rentalList.add(new Rental("1234", "Mini", "Adam", "Adam@123.ie", "14", "20", 10));
+    }
+
+    @PostMapping("/addRentals")
+    public Rental addRental(@RequestBody Rental newRental) {
+
+        rentalList.add(newRental);
+        return newRental;
+
+    }
+    @GetMapping("/getRentals")
+    public List<Rental> getRentalList() {
+
+        return rentalList;
+
+    }
+
+
 
 }
