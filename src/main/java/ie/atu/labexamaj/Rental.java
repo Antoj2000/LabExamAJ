8c10d552509da99ba23d6758e2698dde91879bb5
Antoj2000 <g00385306@atu.ie>
2024-11-05 10:27:16 +0000

    project started

--- src/main/java/ie/atu/labexamaj/Rental.java
+++ src/main/java/ie/atu/labexamaj/Rental.java
@@ -1,5 +1,9 @@
 package ie.atu.labexamaj;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +11,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class Rental {
+
+    @NotBlank(message = "Rental code must be entered")
+    public String rentalCode;
+
+    @Max(value = 50, message = "Max 50 characters")
+    public String carModel;
+
+    @Max(value = 100, message = "Max 100 characters")
+    public String renterName;
+
+    @Email(message = "Must be a valid email format")
+    public String renterEmail;
+
+
+    public String rentalStartDate;
+
+    public String rentalEndDate;
+
+    @Positive(message = "Must be a positive decimal value")
+    public double dailyRate;
 
 }
